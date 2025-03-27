69210c37ebcb5ca407e1c81235a3fa8ecbaf9e9f
kailemia <kailemiakevin@gmail.com>
2025-03-27 09:37:45 +0300

    Testimonials updated and a separate sign up form

---
+++ script.js
@@ -0,0 +1,8 @@
+function onSubmit () {
+    alert ("Form submitted successfully");
+    return true;
+}
+function welcomeMsg(nam) {
+    nam = "Quanese";
+    alert ("Hello " + nam + "!" + " Welcome to Quan Crypto Platform");
+}
